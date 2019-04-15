520f9a5977eecb8d234ad24c6c70054ba6fa1e9d
David Russell <russeldj@clarkson.edu>
2019-04-15 09:42:52 -0400

    Initial code

---
+++ source/simulation.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+#Model params
+MAX_ACCEL = 10
+MAX_DECCEL = -10
+LATENCY = 0
+
+#
+NUM_CARS = 7
+
+
+#Optimizer params
+OP_FOLLOWING_DIST = 5
+JERK = 2
+
+def initialize_cars(num_cars=NUM_CARS):
+    locs = np.linspace()
+
+
+def simulation(op_following_dist=OP_FOLLOWING_DIST, jerk=JERK, max_accell=MAX_ACCEL, max_deccel=MAX_DECCEL, latency=LATENCY):
+
+if __name__ == "__main__":
+    simulation()
+    
