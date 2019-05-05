3f2dcd2443200860acc1a0db457c56654c0a1c0d
David Russell <russeldj@clarkson.edu>
2019-05-05 10:57:24 -0400

    nicely working visualizations

--- source/optimization_examples.py
+++ source/optimization_examples.py
@@ -47,7 +47,7 @@ def black_box_vector_input_plus_penalty(following_jerk): #First the optimizer de
     penalty_cost = penalty(following_jerk)
     return function_cost + penalty_cost
 
-def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False,load=True):
+def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False,load=True,title="plot of time versus parameters"):
     X_MIN = 0
     X_MAX = 1
     Y_MIN = 2
@@ -69,19 +69,35 @@ def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False,load=True):
     plt.colorbar(colors)
     plt.xlabel("following distance")
     plt.ylabel("jerk")
-    plt.title("contour plot of the time taken, averaged over a few iterations")
+    plt.title(title)
     #plt.show() Show at the end
 
+JERK_MIN = 1
+JERK_MAX=5
+FOLLOWING_MAX = 7
+
 def constraint1(x):
-    return np.atleast_1d(x[0] - x[1]) #equal to zero
+    return np.atleast_1d(x[0] - x[1]) #enforce hard breaking when close
 
 def constraint2(x):
-    return np.atleast_1d(x[1] - 1) #greater than or equal to zero
+    return np.atleast_1d(x[1] - JERK_MIN) #make sure the jerk isn't too low, or you'll get strange solutions
 
-def penalty(x, scale_factor=1000):
+def constraint3(x):
+    return np.atleast_1d(JERK_MAX - x[1]) # Upper bound on jerk
+
+def constraint4(x):
+    return np.atleast_1d(FOLLOWING_MAX - x[0]) # upper bound of following dist
+
+def penalty(x, scale_factor=100,power=2):
+    """
+    The scale factor is the weight of the penalty
+    The power is what it's raised to
+    """
     p1 = min(constraint1(x),0)
     p2 = min(constraint2(x),0)
-    return (p1 ** 2 + p2 ** 2) * scale_factor # the sum of the sqared penalties
+    p3 = min(constraint3(x),0)
+    p4 = min(constraint4(x),0)
+    return (p1 ** power + p2 ** power + p3 **power + p4 **power) * scale_factor # the sum of the sqared penalties
 
 def nelder_mead_unconstrained():
     x0 = np.array([6.0,4.0])
@@ -90,25 +106,40 @@ def nelder_mead_unconstrained():
 
 def nelder_mead_penalty():
     x0 = np.array([6.0,4.0])
-    res = minimize(black_box_vector_input, x0, method='nelder-mead', options={'maxiter': 100})
+    res = minimize(black_box_vector_input_plus_penalty, x0, method='nelder-mead', options={'maxiter': 100})
     return res
 
 def plot_func_plus_penalty(load_saved=True):
-    plot(black_box_vector_input_plus_penalty, [0.5,8,0.5,8],10, True, load_saved)
+    plot(black_box_vector_input_plus_penalty, [0.5,8,0.5,8],10, True, load_saved, "Fhunction plus penalty versus parameters, with iterations overlayed")
+
+def plot_penalty(load_saved=False):
+    plot(penalty, [-4.0,FOLLOWING_MAX + 4,-4.0,JERK_MAX + 4],10, True, load_saved, "Plot of penalty versus parameters, with iterations overlayed")
+
+def plot_func(load_saved=False):
+    plot(black_box_vector_input, [0.5,8,0.5,8],10, True, load_saved, "Plot of function versus parameters, with iterations overlayed")
 
 if __name__ == "__main__":
-    x0 = np.array([6.0,4.0])
-    #res = minimize(f, x0, method='SLSQP', constraints=[{"fun": constraint2, "type": "ineq"}])
-    #res = minimize(black_box_vector_input, x0, method='nelder-mead', constraints=[{"fun": constraint1, "type": "ineq"}, {"fun": constraint2, "type": "ineq"}], options={'maxiter': 10})
-    plot_func_plus_penalty(False)
-    res = nelder_mead_unconstrained()
+    # Do the optimization
+    res = nelder_mead_penalty()
     print(res.x)
     accumulator_np = np.array(accumulator)
+    np.save("iterations.npy", accumulator_np)
     print(accumulator_np)
-    colors = cm.rainbow(np.linspace(0, 1, len(accumulator)))
-    plt.scatter(accumulator_np[:,0], accumulator_np[:,1],color=colors)
-    plt.show()
 
-    #plt.legend(loc='upper left')
-    #plot(black_box, [min(accumulator_np[:,0]),max(accumulator_np[:,0]),min(accumulator_np[:,1]),max(accumulator_np[:,1])],40)
-    #plot(black_box_vector_input, [0.5,8,0.5,8],10, True) #replace black_box with rosen2d
+    # Plot it againsts three different backgrounds
+    colors = cm.rainbow(np.linspace(0, 1, len(accumulator)))
+    # the penalty alone
+    plot_penalty(False)
+    plt.scatter(accumulator_np[:,0], accumulator_np[:,1],color=colors)
+    plt.savefig("../results/penalty.png") # up one folder and then results
+    plt.clf() # clear figure
+
+    plot_func(False)
+    plt.scatter(accumulator_np[:,0], accumulator_np[:,1],color=colors)
+    plt.savefig("../results/function.png")
+    plt.clf()
+
+    plot_func_plus_penalty(False)
+    plt.scatter(accumulator_np[:,0], accumulator_np[:,1],color=colors)
+    plt.savefig("../results/function_plus_penalty.png")
+    plt.clf()
