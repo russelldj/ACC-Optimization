d074799472ed885bd2c17140911395283f297603
David Russell <russeldj@clarkson.edu>
2019-05-05 09:38:42 -0400

    somewhat janky but working nelder-mead with penalty

--- source/optimization_examples.py
+++ source/optimization_examples.py
@@ -34,11 +34,20 @@ def black_box_averager(x, y, num_iters=5):
     return value / float(num_iters)
 
 def black_box_vector_input(following_jerk): #First the optimizer determines the best values but they come out into two scalar variables, in order to put it in the black box to evaluate it, it needs to be in vector form which we put it in here
+    #print("running black box vector input with following_jerk = {}".format(following_jerk))
     accumulator.append(following_jerk)  #saves x[i] at current iteration
     following, jerk = following_jerk
     return black_box(following, jerk)
 
-def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False):
+def black_box_vector_input_plus_penalty(following_jerk): #First the optimizer determines the best values but they come out into two scalar variables, in order to put it in the black box to evaluate it, it needs to be in vector form which we put it in here
+    #print("running black box vector input with following_jerk = {}".format(following_jerk))
+    accumulator.append(following_jerk)  #saves x[i] at current iteration
+    following, jerk = following_jerk
+    function_cost = black_box(following, jerk)
+    penalty_cost = penalty(following_jerk)
+    return function_cost + penalty_cost
+
+def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False,load=True):
     X_MIN = 0
     X_MAX = 1
     Y_MIN = 2
@@ -46,37 +55,60 @@ def plot(function_, limits=[0,1,0,1], samples=20, is_vector=False):
     z = np.zeros((samples,samples))
     x_list = np.linspace(limits[X_MIN], limits[X_MAX], samples)
     y_list = np.linspace(limits[Y_MIN], limits[Y_MAX], samples)
-    for j in tqdm(range(samples)): #progess bar
-        for i in range(samples):
-            if is_vector:
-                z[i,j] = function_(np.array([x_list[j], y_list[i]]))
-            else:
-                z[i,j] = function_(x_list[j],y_list[i])
-
-    colors= plt.contour(x_list, y_list, z)
+    if load:
+        z = np.load("z_values.npy")
+    else:
+        for j in tqdm(range(samples)): #progess bar
+            for i in range(samples):
+                if is_vector:
+                    z[i,j] = function_(np.array([x_list[j], y_list[i]]))
+                else:
+                    z[i,j] = function_(x_list[j],y_list[i])
+    np.save("z_values.npy", z) # save these values to avoid recomputing
+    colors = plt.contourf(x_list, y_list, z, 30)
     plt.colorbar(colors)
     plt.xlabel("following distance")
     plt.ylabel("jerk")
     plt.title("contour plot of the time taken, averaged over a few iterations")
-    plt.show()
+    #plt.show() Show at the end
 
 def constraint1(x):
-    return np.atleast_1d(x[0] + x[1]) #equal to zero
+    return np.atleast_1d(x[0] - x[1]) #equal to zero
 
 def constraint2(x):
     return np.atleast_1d(x[1] - 1) #greater than or equal to zero
 
+def penalty(x, scale_factor=1000):
+    p1 = min(constraint1(x),0)
+    p2 = min(constraint2(x),0)
+    return (p1 ** 2 + p2 ** 2) * scale_factor # the sum of the sqared penalties
+
+def nelder_mead_unconstrained():
+    x0 = np.array([6.0,4.0])
+    res = minimize(black_box_vector_input, x0, method='nelder-mead', options={'maxiter': 100})
+    return res
+
+def nelder_mead_penalty():
+    x0 = np.array([6.0,4.0])
+    res = minimize(black_box_vector_input, x0, method='nelder-mead', options={'maxiter': 100})
+    return res
+
+def plot_func_plus_penalty(load_saved=True):
+    plot(black_box_vector_input_plus_penalty, [0.5,8,0.5,8],10, True, load_saved)
+
 if __name__ == "__main__":
     x0 = np.array([6.0,4.0])
-    res = minimize(f, x0, method='SLSQP', constraints=[{"fun": constraint2, "type": "ineq"}])
-
+    #res = minimize(f, x0, method='SLSQP', constraints=[{"fun": constraint2, "type": "ineq"}])
+    #res = minimize(black_box_vector_input, x0, method='nelder-mead', constraints=[{"fun": constraint1, "type": "ineq"}, {"fun": constraint2, "type": "ineq"}], options={'maxiter': 10})
+    plot_func_plus_penalty(False)
+    res = nelder_mead_unconstrained()
     print(res.x)
     accumulator_np = np.array(accumulator)
     print(accumulator_np)
-    pdb.set_trace()
     colors = cm.rainbow(np.linspace(0, 1, len(accumulator)))
     plt.scatter(accumulator_np[:,0], accumulator_np[:,1],color=colors)
+    plt.show()
 
     #plt.legend(loc='upper left')
     #plot(black_box, [min(accumulator_np[:,0]),max(accumulator_np[:,0]),min(accumulator_np[:,1]),max(accumulator_np[:,1])],40)
-    plot(f, [-8,8,-8,8],20, True) #replace black_box with rosen2d
+    #plot(black_box_vector_input, [0.5,8,0.5,8],10, True) #replace black_box with rosen2d
